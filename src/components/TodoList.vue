b6fa042e5bdcafabfe5d2748a550d7a9114e257f
Richard Ward <richard.ward637@outlook.com>
2021-08-19 14:16:36 +0100

    Fix delete button

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,7 +5,12 @@
     <ion-list v-for="todo of items" :key="todo.id">
       <ion-item>
         <ion-label>{{ todo.value }}</ion-label>
-        <ion-checkbox slot="start"></ion-checkbox>
+        <ion-checkbox
+            v-bind:data-id="todo.id"
+            slot="start"
+            v-model="checkedMap[todo.id]"
+            @click="currentCheckId = todo.id"
+        ></ion-checkbox>
       </ion-item>
     </ion-list>
   </div>
@@ -19,7 +24,23 @@ import {TodoItems} from "@/interfaces";
 export default defineComponent({
   name: 'TodoList',
   props: {
-    items: Array as PropType<Array<TodoItems>>,
+    items: Array as PropType<Array<TodoItems>>
+  },
+  data: () => ({
+    checkedMap: {},
+    currentCheckId: 0
+  }),
+  watch: {
+    checkedMap: {
+      handler: function (oldValue, newValue) {
+        if (newValue) {
+          const value = newValue[this.currentCheckId];
+          this.$emit('checkboxClicked', {id: this.currentCheckId, checked: value});
+        }
+      },
+      deep: true,
+      immediate: true
+    }
   },
   components: {
     IonList,
--- src/views/Home.vue
+++ src/views/Home.vue
@@ -1,24 +1,39 @@
 <template>
   <div class="home">
-    <AddForm v-bind:items="todoItems" @todoSubmit="todoItems.push({id: todoItems.slice().reverse()[0].id + 1, value: $event})" />
-    <TodoList v-bind:items="todoItems" />
+    <AddForm v-bind:items="todoItems" @todoSubmit="todoSubmit($event)" @todoDelete="deleteButtonClicked($event)" />
+    <TodoList v-bind:items="todoItems" @checkboxClicked="checkboxClicked($event)" />
   </div>
 </template>
 
 <script lang="ts">
-  import TodoList from '@/components/TodoList.vue';
-  import {defineComponent} from "vue";
-  import AddForm from "@/components/AddForm.vue";
+import TodoList from '@/components/TodoList.vue';
+import {defineComponent} from "vue";
+import AddForm from "@/components/AddForm.vue";
 
-  export default defineComponent({
+export default defineComponent({
     name: 'Home',
     components: {
       AddForm,
       TodoList
     },
     data: () => ({
-      todoItems: [{id: 0, value: 'test'}, {id: 1, value: 'foo'}]
+      todoItems: [{id: 0, value: 'test', checked: false}, {id: 1, value: 'foo', checked: false}]
     }),
+    methods: {
+      checkboxClicked(event: any) {
+        const index = this.todoItems.findIndex(item => item.id === event.id);
+        this.todoItems[index].checked = event.checked;
+      },
+      deleteButtonClicked(event: any) {
+        this.todoItems.filter(item => item.checked).forEach(item => {
+          this.todoItems.splice(this.todoItems.findIndex(todoItem => todoItem.id === item.id), 1);
+        });
+      },
+      todoSubmit(event: any) {
+        const lastTodo = this.todoItems.slice().reverse()[0];
+        this.todoItems.push({id: lastTodo.id + 1, value: event, checked: lastTodo.checked});
+      }
+    },
     setup() {
       return {
 
